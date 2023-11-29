b528804c56d786e25b206d712656f6c5de02767f
Oliver Hess <oliver.hess@student.hpi.uni-potsdam.de>
2023-11-29 15:50:24 +0100

    Add file statistics to the view panel
    To get an overview how many junitxml files are actually there :)

--- results-browser/src/ResultsBrowser.jsx
+++ results-browser/src/ResultsBrowser.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 import FileBrowser from "react-keyed-file-browser";
 import "./ResultsBrowser.css";
-import { getPlainFiles } from "./resultsUtil";
+import { getFileStatistics, getPlainFiles } from "./resultsUtil";
 
 const ResultsBrowser = () => {
   const [resultData, setResultData] = useState([]);
@@ -13,10 +13,24 @@ const ResultsBrowser = () => {
       .catch(error => console.error(error.message))
   }, []);
 
-  const plainFiles = useMemo(() => getPlainFiles(resultData), [resultData]);
+  const { plainFiles, totalCount } = useMemo(() => getPlainFiles(resultData), [resultData]);
+  const fileCounts = useMemo(() => getFileStatistics(plainFiles), [plainFiles]);
 
   return (
     <div className="results-browser">
+      <table className="results-statistics">
+        <thead>
+          <tr><th>Subject</th><th>Count</th></tr>
+        </thead>
+        <tbody>
+          <tr><td>packages</td><td>{ totalCount }</td></tr>
+          {
+            Object.entries(fileCounts).map(([file, count]) => (
+              <tr key={ file }><td>{ file }</td><td>{ count }</td></tr>
+            ))
+          }
+        </tbody>
+      </table>
       <FileBrowser
         files={ plainFiles }
         canFilter
--- results-browser/src/resultsUtil.jsx
+++ results-browser/src/resultsUtil.jsx
@@ -1,18 +1,36 @@
 import Moment from "moment";
 
 export const getPlainFiles = (resultData) => {
-  if (resultData.length === 0) return [];
+  if (resultData.length === 0) return { plainFiles: [], totalCount: 0 };
 
   // First directory is "." (the results dir)
   const resultsDirectories = resultData[0].contents;
-  return resultsDirectories.flatMap(dir => {
-    if (!dir.contents) return [];
+  return {
+    plainFiles: resultsDirectories.flatMap(dir => {
+      if (!dir.contents) return [];
 
-    // Flatmap files of each directory
-    return dir.contents.map(file => ({
-      key: file.name.substring((2)), // Cut "./" from the file name
-      modified: +Moment(file.time),
-      size: file.size,
-    }))
-  })
+      // Flatmap files of each directory
+      return dir.contents.map(file => ({
+        key: file.name.substring((2)), // Cut "./" from the file name
+        modified: +Moment(file.time),
+        size: file.size,
+      }));
+    }),
+    totalCount: resultsDirectories.length,
+  };
+};
+
+export const getFileStatistics = (plainFiles) => {
+  if (plainFiles.length === 0) return {};
+
+  const getFileStatistic = (file) => {
+    return plainFiles.filter(plainFile => plainFile.key.endsWith(file)).length;
+  };
+
+  return {
+    "cpython-test.log": getFileStatistic("cpython-test.log"),
+    "cpython-test-results.xml": getFileStatistic("cpython-test-results.xml"),
+    "graalpy-test.log": getFileStatistic("graalpy-test.log"),
+    "graalpy-test-results.xml": getFileStatistic("graalpy-test-results.xml"),
+  };
 };
