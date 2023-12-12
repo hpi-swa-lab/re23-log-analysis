1eb1b1d2e8bec80ab1e2e5d5731a8b3a683f68d3
antonykamp <info@antonykamp.de>
2023-12-12 15:55:48 +0100

    feat: add grouping to analyzer

--- analyse/ErrorAnalyzer.py
+++ analyse/ErrorAnalyzer.py
@@ -1,7 +1,7 @@
 from JunitXMLParser import JunitXMLParser
 import os
+from functools import reduce
 from ErrorDocument import ErrorDocument
-from collections import Counter
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from utils import tokenize
@@ -55,25 +55,62 @@ class ErrorAnalyzer(object):
     def filter_packages(self, package):
         filter_function = lambda errorDocument: errorDocument.packageName is not None and re.search(package, errorDocument.packageName, re.IGNORECASE) is not None
         return self._filter(filter_function)
+    
+    def _group(self, grouping_function, error_property, grouping_value):
+        # Collect all error documents that match the grouping function
+        grouped_error_documents = [errorDocument for errorDocument in self.graalpy_error_documents if grouping_function(errorDocument)]
+        grouped_error_names = [errorDocument.name for errorDocument in grouped_error_documents]
+        # Collect all error documents that match the grouping function and are also in the cpython error documents to avoid double counting
+        grouped_cpython_error_documents = [errorDocument for errorDocument in self.cpython_error_documents if errorDocument.name in grouped_error_names]
+        # Remove grouped error documents from graalpy error documents
+        graalpy_error_documents_without_group = [errorDocument for errorDocument in self.graalpy_error_documents if errorDocument.name not in grouped_error_names]
+        group_size = len(grouped_error_documents) - len(grouped_cpython_error_documents)
+        # Create a new error document that represents the group and add it to the graalpy error documents
+        new_graalpy_error_document = ErrorDocument(**{error_property: grouping_value, "value": group_size, "name": "group_{}_{}".format(error_property, grouping_value)})
+        return ErrorAnalyzer(self.data_collector, [*graalpy_error_documents_without_group, new_graalpy_error_document], self.cpython_error_documents)
+
+    def group_error_type(self, error_type):
+        grouping_function = lambda errorDocument: errorDocument.errorType is not None and re.search(error_type, errorDocument.errorType, re.IGNORECASE) is not None
+        return self._group(grouping_function, "errorType", error_type)
+
+    def group_error_message(self, error_message):
+        grouping_function = lambda errorDocument: errorDocument.errorMessage is not None and re.search(error_message, errorDocument.errorMessage, re.IGNORECASE) is not None
+        return self._group(grouping_function, "errorMessage", error_message)
+    
+    def group_last_lines(self, last_line):
+        grouping_function = lambda errorDocument: errorDocument.last_stacktrace_line is not None and re.search(last_line, errorDocument.last_stacktrace_line, re.IGNORECASE) is not None
+        return self._group(grouping_function, "stackTrace", last_line)
+    
+    def group_packages(self, package):
+        grouping_function = lambda errorDocument: errorDocument.last_stacktrace_line is not None and re.search(package, errorDocument.package, re.IGNORECASE) is not None
+        return self._group(grouping_function, "package", package)
+
+    def _count(agg, curr):
+        error, value = curr
+        if error in agg:
+            agg[error] += value
+        else:
+            agg[error] = value
+        return agg
 
     def count_error_types(self):
-        error = [tokenize(errorDocument.errorType) for errorDocument in self.error_documents]
-        error_counts = Counter(error)
+        error = [(tokenize(errorDocument.errorType), errorDocument.value) for errorDocument in self.error_documents]
+        error_counts = reduce(ErrorAnalyzer._count, error, dict())
         return error_counts
     
     def count_error_messages(self):
-        error = [tokenize(errorDocument.errorMessage) for errorDocument in self.error_documents]
-        error_counts = Counter(error)
+        error = [(tokenize(errorDocument.errorMessage), errorDocument.value) for errorDocument in self.error_documents]
+        error_counts = reduce(ErrorAnalyzer._count, error, dict())
         return error_counts
     
     def count_packages(self):
-        packages = [errorDocument.packageName for errorDocument in self.error_documents]
-        package_counts = Counter(packages)
-        return package_counts
+        packages = [(tokenize(errorDocument.package), errorDocument.value) for errorDocument in self.error_documents]
+        packages_counts = reduce(ErrorAnalyzer._count, packages, dict())
+        return packages_counts
 
     def count_last_lines(self):
-        last_lines = [tokenize(errorDocument.last_stacktrace_line) for errorDocument in self.error_documents]
-        last_line_counts = Counter(last_lines)
+        last_lines = [(tokenize(errorDocument.last_stacktrace_line), errorDocument.value) for errorDocument in self.error_documents]
+        last_line_counts = reduce(ErrorAnalyzer._count, last_lines, dict())
         return last_line_counts
     
     def _calculate_similarity(self, errors):
--- analyse/ErrorDocument.py
+++ analyse/ErrorDocument.py
@@ -1,11 +1,12 @@
 class ErrorDocument(object):
     
-    def __init__(self, name, packageName, errorType, errorMessage, stackTrace):
+    def __init__(self, name = "", packageName = "", errorType = "", errorMessage = "", stackTrace = "", value=1):
         self.packageName = packageName
         self.errorType = errorType
         self.errorMessage = errorMessage
         self.stackTrace = stackTrace
         self.name = name
+        self.value = value
 
     @property
     def last_stacktrace_line(self):
@@ -16,5 +17,5 @@ class ErrorDocument(object):
         return self.stackTrace.split('\n')[i]
 
     def __str__(self):
-        return "{}({} # {}): {}\n{}".format(self.errorType,self.packageName, self.name, self.errorMessage, self.stackTrace)
+        return "{}x{}({} # {}): {}\n{}".format(self.value, self.errorType,self.packageName, self.name, self.errorMessage, self.stackTrace)
   
