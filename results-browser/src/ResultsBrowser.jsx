a94956b593a9254bdbcd11820eee04918151e236
Oliver Hess <oliver.hess@student.hpi.uni-potsdam.de>
2023-12-15 18:52:10 +0100

    Enable regex filter on content
    - load all file contents into memory at start
    - use regex filter on file contents
    - also update the file counts on the left side based on the filter

--- results-browser/src/ResultsBrowser.jsx
+++ results-browser/src/ResultsBrowser.jsx
@@ -1,38 +1,65 @@
 import React, { useEffect, useMemo, useState } from "react";
 import FileBrowser from "react-keyed-file-browser";
 import "./ResultsBrowser.css";
-import { getFileStatistics, getPlainFiles } from "./resultsUtil";
+import { getFileStatistics, getFlattenedFiles } from "./resultsUtil";
 import FileViewer from "./FileViewer";
 
 const ResultsBrowser = () => {
+  const [loading, setLoading] = useState(true);
   const [resultsIndexData, setResultsIndexData] = useState([]);
   const [selectedFile, setSelectedFile] = useState(undefined);
-  const [selectedFileContent, setSelectedFileContent] = useState(undefined);
+  const [filesWithContent, setFilesWithContent] = useState([]);
+  const [filterRegexInput, setFilterRegexInput] = useState(undefined);
 
   useEffect(() => {
+    // Load index of result files
     fetch("results/results_index.json")
       .then(response => response.json())
       .then(data => setResultsIndexData(data))
       .catch(error => console.error(error.message))
   }, []);
 
-  useEffect( () => {
-    if (!selectedFile) return;
-    fetch(`results/${selectedFile.key}`)
-      .then(response => response.text())
-      .then(data => {
-        setSelectedFileContent(data);
-      })
-      .catch(error => console.error(error.message));
-  }, [selectedFile]);
+  // Flat down directory structure
+  const flattenedFiles = useMemo(() => getFlattenedFiles(resultsIndexData), [resultsIndexData]);
 
-  const { plainFiles, totalCount } = useMemo(() => getPlainFiles(resultsIndexData), [resultsIndexData]);
-  const fileCounts = useMemo(() => getFileStatistics(plainFiles), [plainFiles]);
+  // Enrich all result files with their contents
+  useEffect(() => {
+    if (flattenedFiles.length === 0) return [];
+
+    Promise.all(
+      flattenedFiles.map(async file => {
+        console.log(`Loading ${file.key}`);
+        const response = await fetch(`results/${file.key}`)
+        const textContent = await response.text();
+        return { ...file, content: textContent };
+      })
+    ).then(resultFiles => {
+      setLoading(false);
+      setFilesWithContent(resultFiles);
+    });
+  }, [flattenedFiles]);
+
+  // Filter files
+  const filteredFiles = useMemo(() => {
+    if (!filterRegexInput) return filesWithContent;
+    const filterRegex = new RegExp(filterRegexInput.toString());
+    const regexPredicate = filesWithContent => filesWithContent.content.match(filterRegex);
+    return filesWithContent.filter(regexPredicate)
+  }, [filesWithContent, filterRegexInput]);
+
+  const filterFiles = (event) => {
+    setSelectedFile(undefined);
+    setFilterRegexInput(event.target.value);
+  };
+
+  const fileCounts = useMemo(() => getFileStatistics(filteredFiles), [filteredFiles]);
 
   const selectNewFile = (file) => {
-    setSelectedFileContent(undefined);
-    setSelectedFile(file);
+    const fileWithContent = filesWithContent.find(fileWithContent => fileWithContent.key === file.key);
+    setSelectedFile(fileWithContent);
   };
+
+  if (loading) return "Loading files...";
 
   return (
     <div className="results-browser">
@@ -41,7 +68,6 @@ const ResultsBrowser = () => {
           <tr><th>Subject</th><th>Count</th></tr>
         </thead>
         <tbody>
-          <tr><td>packages</td><td>{ totalCount }</td></tr>
           {
             Object.entries(fileCounts).map(([file, count]) => (
               <tr key={ file }><td>{ file }</td><td>{ count }</td></tr>
@@ -49,14 +75,21 @@ const ResultsBrowser = () => {
           }
         </tbody>
       </table>
-      <FileBrowser
-        files={ plainFiles }
-        onSelectFile={ selectNewFile }
-        canFilter
-        showFoldersOnFilter
-        detailRenderer={ () => null }
-      />
-      { selectedFile && selectedFileContent && <FileViewer file={ { ...selectedFile, content: selectedFileContent } } /> }
+      <div className="fileBrowserWithFilter">
+        <input
+          type="text"
+          placeholder="Filter files based on Regex"
+          onChange={ filterFiles }
+        />
+        <FileBrowser
+          files={ filteredFiles }
+          onSelectFile={ selectNewFile }
+          canFilter={ false } // disable file browsers own filters
+          noFilesMessage="No files available or matching"
+          detailRenderer={ () => null }
+        />
+      </div>
+      { selectedFile && filesWithContent && <FileViewer file={ selectedFile } /> }
     </div>
   );
 }
